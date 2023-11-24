ae1c77dbb270dbe3500c4baef7de38b548145bbd
João Victor Bastos Botelho <121052506+Dust-bat@users.noreply.github.com>
2023-11-24 08:11:02 -0300

    correcao2

--- src/main/java/mypackages/DockerMessageController.java
+++ src/main/java/mypackages/DockerMessageController.java
@@ -1,5 +1,8 @@
 package mypackages;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class DockerMessageController {
 @GetMapping("/messages")
     public String getMessage() {
